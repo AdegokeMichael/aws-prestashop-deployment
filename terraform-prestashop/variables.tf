68db2af00c8f065e7b159192037196c02a3dfd7e
AdegokeMichael <michaelbeatus.mia@gmail.com>
2025-07-22 11:00:14 +0100

    Add variables.tf to manage AWS infrastructure inputs for Prestashop deployment

---
+++ terraform-prestashop/variables.tf
@@ -0,0 +1,35 @@
+variable "region" {
+	description = "The AWS region where resources will be deployed"
+	type = string
+}
+
+variable "vpc_id" {
+  description = "The ID of the VPC to attach the Security Group"
+  type        = string
+}
+
+
+variable "instance_type" {
+	description = "EC2 instance type"
+	type = string
+}
+
+variable "ami_id" {
+	description = "The AMI ID for the EC2 instance"
+	type = string
+}
+
+variable "key_name" {
+	description = "EC2 key pair name"
+	type = string
+}
+
+variable "private_key_path" {
+	description = "path to your public key path"
+	type = string
+}
+
+variable "vpc_id" {
+	description = "The ID of the VPC attached to the security group"
+	type = string
+}
